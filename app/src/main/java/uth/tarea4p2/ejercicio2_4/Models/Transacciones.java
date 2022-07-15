c94634c9436df060b7ee54f354dda7ce17c56406
CarlosCanales201810010580 <79998068+CarlosCanales201810010580@users.noreply.github.com>
2022-07-15 13:51:45 -0600

    Initial commit

---
+++ app/src/main/java/uth/tarea4p2/ejercicio2_4/Models/Transacciones.java
@@ -0,0 +1,19 @@
+package uth.tarea4p2.ejercicio2_4.Models;
+
+public class Transacciones
+{
+
+    public static final String NameDatabase="DBPM01";
+
+    public static final String TablaSignaturess="Signaturess";
+
+    public static final String id = "id";
+    public static final String descripcion="descripcion";
+    public static final String firma="firma";
+
+
+    public static final String CreateTableSignaturess ="CREATE TABLE IF NOT EXISTS "+ TablaSignaturess + "(id INCREMENTE PRIMARY KEY, descripcion TEXT,"+"firma BLOB);";
+
+    public static final String DROPTableSignaturess ="DROP TABLE IF EXISTS " + TablaSignaturess;
+
+}
